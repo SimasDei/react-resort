15e219983c2602bbffea16099d4d0b3b0431dab4
Simas <simonas.deimantavicius@mediapark.com>
2019-08-05 11:22:31 +0300

    implement navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,21 +1,26 @@
 import React from 'react';
-import {Switch, Route} from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import Home from './pages/Home';
 import Rooms from './pages/Rooms';
 import SingleRoom from './pages/SingleRoom';
-import Error from './pages/Error';
+import Error404 from './pages/Error';
+
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path={'/'} component={Home} />
-      <Route exact path={'/rooms'} component={Rooms} />
-      <Route exact path={'/room/:id'} component={SingleRoom} />
-      <Route component={Error} />
-    </Switch>
+    <>
+      <Navbar />
+      <Switch>
+        <Route exact path={'/'} component={Home} />
+        <Route exact path={'/rooms'} component={Rooms} />
+        <Route exact path={'/room/:slug'} component={SingleRoom} />
+        <Route component={Error404} />
+      </Switch>
+    </>
   );
 }
 
