fd3a740c232771616818f31d8f844595409b41d3
Simas <simonas.deimantavicius@mediapark.com>
2019-08-06 10:56:39 +0300

    add Services container and Title component

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -3,16 +3,20 @@ import { Link } from 'react-router-dom';
 
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
+import Services from '../components/Services';
 
 const Home = () => {
   return (
-    <Hero>
-      <Banner title="luxurious rooms" subtitle="deluxe experience starting at $299">
-        <Link className="btn-primary" to={'/rooms'}>
-          our rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero>
+        <Banner title="luxurious rooms" subtitle="deluxe experience starting at $299">
+          <Link className="btn-primary" to={'/rooms'}>
+            our rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </>
   );
 };
 
